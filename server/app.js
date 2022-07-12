a91b1fed0a2510e83ffbbe10f122fde8bd885c24
imdarsh <shindesahil63@gmail.com>
2022-07-13 00:58:04 +0530

    Backend API

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const connectDB = require('./db/connect');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use('/api/users', require('./routes/authRoutes'));
+
+const port = process.env.PORT || 7000;
+
+const start = async () => {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+}
+
+start();
